7d620396ab4275cc946afeda779ac3ff097a8cd6
zjucx <zju.chx@gmail.com>
2016-10-23 01:52:31 +0800

    add crawler for sina

--- src/scrawler/sinaLogin.go
+++ src/scrawler/sinaLogin.go
@@ -3,3 +3,17 @@ package scrawler
 import (
   "fmt"
 )
+
+/*
+ * open main page and you should get cookie and save
+ */
+ func getCookies() (strCookies string) {
+   strLoginUrl := `http://weibo.com/login.php`
+   _, strCookies := DoRequest(`GET`, strLoginUrl, ``, ``, ``, nil)
+ }
+
+/*
+ * when finish inputing the username, send the prelogin req
+ * you can get login info for logining sina
+ */
+func sendPreLogin
